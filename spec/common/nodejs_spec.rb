767c8f9fd8d823ef7201a8d7d9f2db5f775cf327
Takakura Yusuke <takakura.yusuke@gmail.com>
2018-12-15 15:38:26 +0900

    Update Software Settings.

--- spec/common/nodejs_spec.rb
+++ spec/common/nodejs_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 target_versions = [
-  'v5.12.0',
-  'v6.9.3'
+  'v10.14.2'
 ]
 target_versions.each do|version|
   describe file(home_path + '/.nodebrew/node/' + version) do
