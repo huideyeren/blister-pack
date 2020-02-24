35cacfe5dc78f192538941e47a00bee9822479a3
Iosif Takakura <iosif@Iosif-no-MacBook-Pro.local>
2020-02-24 23:51:10 +0900

    add java

---
+++ spec/common/java_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+target_packages = [
+  'java',
+]
+
+target_packages.each do |package|
+  describe command "brew cask list | grep -q #{package}" do
+    its(:exit_status) { should eq 0 }
+  end
+end
--- spec/common/python_spec.rb
+++ spec/common/python_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-target_packages = %w(python python3 pyenv)
+# target_packages = %w(python python3 pyenv)
 
-target_packages.each do|package|
-  describe file(homebrew_cellar + '/' + package) do
-    it { should be_directory }
-  end
-end
+# target_packages.each do|package|
+#   describe file(homebrew_cellar + '/' + package) do
+#     it { should be_directory }
+#   end
+# end
 
-describe command('which pip') do
-  its(:stdout) { should match(%r{/usr/local/bin/pip}) }
-end
-describe command('which virtualenv') do
-  its(:stdout) { should match(%r{/usr/local/bin/virtualenv}) }
-end
-describe command('which pipenv') do
-  its(:stdout) { should match(%r{/usr/local/bin/pipenv}) }
-end
-describe command('which pipenv') do
-  its(:stdout) { should match(%r{/usr/local/bin/pipenv}) }
-end
+# describe command('which pip') do
+#   its(:stdout) { should match(%r{/usr/local/bin/pip}) }
+# end
+# describe command('which virtualenv') do
+#   its(:stdout) { should match(%r{/usr/local/bin/virtualenv}) }
+# end
+# describe command('which pipenv') do
+#   its(:stdout) { should match(%r{/usr/local/bin/pipenv}) }
+# end
+# describe command('which pipenv') do
+#   its(:stdout) { should match(%r{/usr/local/bin/pipenv}) }
+# end
